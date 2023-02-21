d19c5a255e07a6d716211d8de0384e76b296c412
Khang Nguyen <62628865+khangaerospace@users.noreply.github.com>
2023-02-21 13:24:04 -0500

    calculation
    add in poison ratio and young modulus.

--- lab1.m
+++ lab1.m
@@ -1,12 +1,12 @@
 clear all;
-file = readmatrix("specimen1.dat");
-specnum = 1;
+file = readmatrix("specimen1.txt"); % out the speciment data you want %
+specnum = 1; % Speciment number %
 time = file(:,1);
 force = file(:,2);
 displace = file(:,3);
 lasdisp = file(:,4);
 gauge1 = file(:,5);
-gause2 = file(:,6);
+gauge2 = file(:,6);
 % right now, I will calculate stress using the initial area, I will add in
 % poison ratio later
 width = [14.87, 14.85, 14.95, 14.87,15.23]; % Define the dimensions of the specimen
@@ -29,18 +29,32 @@ for i = 2:length(force)
         slope_i = mean(diff(gauge1(i-n_start:i))./diff(lasdisp(i-n_start:i)));
         if abs(slope_i - slope_start) > 0.05 * abs(slope_start)
             % If the strain gauge has broken, use laser displacement data to calculate the strain
-            strain(i) = strain(i-1) + (lasdisp(i)-lasdisp(i-1))/lasdisp(1);
+            strain(i) = strain(i-1) + (lasdisp(i)-lasdisp(i-1))/lasdisp(i-1);
             continue
         end
     end
     
     % Otherwise, use the strain gauge data to calculate the strain
-    strain(i) = gauge1(i);
+    strain(i) = abs(gauge1(i));
+    dx = abs(gauge1(i));
+    dy = abs(gauge2(i));
+    linestress = stress(i); % for calculating the stress before yeilding
 end
 
+% find the poisson ratio
+poisson = dx ./ dy;
+avg_poison = mean(poisson);
+disp('Poisson Ratio of speciment');
+disp(avg_poison);
+
+% find the Young's mododulus
+E = linestress ./dx;
+E_avg = mean(E);
+disp('Young modulus of speciment');
+disp(E_avg);
+
+% plot stress and strain
 plot(strain,stress)
 xlabel('Strain');
 ylabel('Stress');
 title('Stress-Strain Curve for Specimen 1');
-
-
