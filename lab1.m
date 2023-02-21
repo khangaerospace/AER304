72158ed0e8cc811b03b2e2e57d717b27f6e79d79
Khang Nguyen <62628865+khangaerospace@users.noreply.github.com>
2023-02-21 01:38:37 -0500

    more testing

--- lab1.m
+++ lab1.m
@@ -1,5 +1,5 @@
 clear all;
-file = readmatrix("specimen2.dat");
+file = readmatrix("specimen1.dat");
 specnum = 1;
 time = file(:,1);
 force = file(:,2);
@@ -9,25 +9,27 @@ gauge1 = file(:,5);
 gause2 = file(:,6);
 % right now, I will calculate stress using the initial area, I will add in
 % poison ratio later
-width = [14.87, 14.85, 14.95, 14.87,15.23];
+width = [14.87, 14.85, 14.95, 14.87,15.23]; % Define the dimensions of the specimen
 thick = [3.22, 3.20, 3.10, 3.25, 3.32];
 A = width.*thick;
 
 stress = zeros(1,length(force));
 strain = zeros(1,length(force));
 strain(1) = gauge1(1); 
-stress(1) = force(1)/A(specnum);
-for i = 2:length(force)
-    stress(i) = force(i)/A(specnum);
-    if gauge1(i-1) == 0.0217322*10-3
-        strain(i) = (lasdisp(i)-lasdisp(i-1))/lasdisp(i);
-    else 
-        strain(i) = abs(gauge1(i));  
+stress = force./A(specnum);
+for i = 2:length(force) 
+    if gauge1(i) > 0.02 && i > 2 % could use a better testing value
+        % If the strain gauge is broken, use laser displacement data to calculate the strain
+        strain(i) = strain(i-1) + (lasdisp(i)-lasdisp(i-1))/lasdisp(1);
+    else
+        % Otherwise, use the strain gauge data to calculate the strain
+         strain(i) = gauge1(i);
     end
-    
-    
 end
 
 plot(strain,stress)
+xlabel('Strain');
+ylabel('Stress');
+title('Stress-Strain Curve for Specimen 1');
 
 
