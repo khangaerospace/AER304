943961cf1fc355d49573d614edc0364b5ade2a2e
Khang Nguyen <62628865+khangaerospace@users.noreply.github.com>
2023-03-22 23:44:50 -0400

    ibeam

--- lab2/aluniumbeam.m
+++ lab2/aluniumbeam.m
@@ -9,11 +9,11 @@ time1 = file(:,1); %[s]
 MTSforce1 = file(:,2); %[N]
 MTSdisp1 = file(:,3); %[mm]
 LD1 = file(:,4); %[mm]
-SG11 = file(:,5); %[mm/mm]
-SG21 = file(:,6); %[mm/mm]
-SG31 = file(:,7); %[mm/mm]
+SG11 = -1*file(:,5); %[mm/mm]
+SG21 = -1*file(:,6); %[mm/mm]
+SG31 = -1*file(:,7); %[mm/mm]
 SG451 = file(:,8); %[mm/mm]
-ActualLD1 = file(:,9); %[mm/mm]
+ActualLD1 = -1*file(:,9); %[mm/mm]
 
 % now create variable for 4 points load
 time2 = file2(:,1); %[s]
@@ -26,6 +26,15 @@ SG32 = file2(:,7); %[mm/mm]
 SG452 = file2(:,8); %[mm/mm]
 ActualLD2 = file2(:,9); %[mm/mm]
 
+%draw line of best fit for 3 points bending
+coef11 = polyfit(ActualLD1, SG11, 1);
+coef21 = polyfit(ActualLD1, SG21, 1);
+coef31 = polyfit(ActualLD1, SG31, 1);
+dis1 = linspace(0, max(ActualLD1), 1000);
+yFit1 = polyval(coef11 , dis1);
+yFit2 = polyval(coef21 , dis1);
+yFit3 = polyval(coef31 , dis1);
+
 % find stress for 3 points bending
 figure(1);
 plot(ActualLD1,SG11);
@@ -33,10 +42,21 @@ hold on
 plot(ActualLD1,SG21);
 hold on
 plot(ActualLD1,SG31);
+hold on
+plot(dis1, yFit1, 'k-');
+hold on
+plot(dis1, yFit2, 'k-');
+hold on
+plot(dis1, yFit3, 'k-');
 legend({'Strain Gauge 1', 'Strain Gauge 2', 'Strain Gauge 3'},'Location','southeast')
 xlabel('Displacement')
 ylabel('Strain')
 title('Displacement vs strain for 3 points bending')
+hold off
+
+
+
+
 
 
 
